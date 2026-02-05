921d26965ba4f2c09ad6056026bef420f28087f8
bubbleyog <bubbleyog@outlook.com>
2026-02-05 15:23:13 +0800

    Initial Astro site with GitHub Pages deployment

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://bubbleyog.github.io',
+  output: 'static',
+});
